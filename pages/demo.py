528ec82d542602f5ada1aabcc06994d28e57b176
Seung Hoon <seunghoon0821@gmail.com>
2022-02-07 00:49:16 +0900

    Final push

--- pages/demo.py
+++ pages/demo.py
@@ -1,50 +1,79 @@
 import os
+import shutil
 import subprocess
-import cv2
-import time
-import numpy as np
+
 import streamlit as st
 import moviepy.editor as mp
-import base64 
-from moviepy.audio.AudioClip import AudioArrayClip
 from moviepy.audio.io.AudioFileClip import AudioFileClip
-from pages.google_api_handler import speech_to_text, translate_text, text_to_speech
+
+from pages.google_api_handler import speech_to_text, translate_text, text_to_speech, langs, get_language_codes
 
 
-temp_input_video_path = "temp.mp4"
-# temp_audio_path = "google.wav"
-temp_output_video_path = "temp_output.mp4"
+tmpDir = "temp"
+temp_input_video_path = os.path.join(tmpDir, "temp_input.mp4")
+temp_source_audio_path = os.path.join(tmpDir, "temp_source.mp3")
+temp_target_audio_path = os.path.join(tmpDir, "temp_target.mp3")
+temp_processed_audio_path = os.path.join(tmpDir, "temp_output.wav")
+temp_output_video_path = os.path.join(tmpDir, "temp_output.mp4")
+
 chunk_duration = 5
 
+def make_silence_audio(duration):
+    duration = (duration > 0) * duration  
+    return mp.AudioClip(make_frame = lambda t: 0, duration=duration, fps=44100)
+
 def process_audio(src_audio, tgt_audio):
-    temp_target_path = "asdf.wav"
-    tgt_audio.write_audiofile(temp_target_path)
+    # Add silent padding
+    temp_src_audio = AudioFileClip(src_audio)
+    silence_duration = (temp_src_audio.duration < 5) * (5-temp_src_audio.duration)
+    silence = mp.AudioClip(make_frame = lambda t: 0, duration=0.5+silence_duration, fps = 44100)
+    temp_src_audio = mp.concatenate_audioclips([silence,temp_src_audio])
+    temp_src_audio.write_audiofile(src_audio)
+    
+    tgt_audio.write_audiofile(temp_target_audio_path)
+    popen = subprocess.Popen(f"python3 audio_handler/FragmentVC/convert.py -w audio_handler/wav2vec_small.pt -v audio_handler/vocoder.pt -c audio_handler/fragmentvc.pt {src_audio} {temp_target_audio_path} {temp_target_audio_path} {temp_target_audio_path} -o {temp_processed_audio_path}", shell=True)
+    popen.wait()
+    return AudioFileClip(temp_processed_audio_path)
 
-    # popen = subprocess.Popen(f"python3 audio_handler/FragmentVC/convert.py -w audio_handler/wav2vec_small.pt -v audio_handler/vocoder.pt -c audio_handler/fragmentvc.pt --sample_rate 44100 {src_audio} {temp_target_path} -o output.wav", shell=True)
-    # # popen = subprocess.Popen(f"python3 audio_handler/FragmentVC/convert.py -w audio_handler/wav2vec_small.pt -v audio_handler/vocoder.pt -c audio_handler/fragmentvc.pt {src_audio} {temp_target_path}", shell=True)
-    # popen.wait()
-
-    os.system(f"python3 audio_handler/FragmentVC/convert.py -w audio_handler/wav2vec_small.pt -v audio_handler/vocoder.pt -c audio_handler/fragmentvc.pt {src_audio} {temp_target_path}")
-    time.sleep(120)
-
-    return AudioFileClip("output.wav")
-
+def resize_audio(audio, target_length):
+    temp_clip = mp.ColorClip(size=(1,1), color=(0,0,0), duration=audio.duration)
+    temp_clip = temp_clip.set_audio(audio)
+    temp_clip = temp_clip.fx(mp.vfx.speedx, audio.duration/target_length)
+    audio = temp_clip.audio
+    return audio
 
 def split_text(text):
-    return [text]
-    # words = text.split()
-    # output = []
+    # Get list of words
+    words = text.results[0].alternatives[0].words
+    start_time = words[0].start_time.seconds
+    splitted_text = []
+    start_times = [start_time]
+    temp_str = ""
+    for w in words:
+        if w.start_time.seconds >= start_time+5:
+            splitted_text.append(temp_str)
+            temp_str = ""
+            start_time = w.start_time.seconds
+            start_times.append(start_time)
+        temp_str += (w.word.split("|")[0])
 
-    # word_length = len(words)
-    # for i in range(len(words)/2 - 1):
-    #     output.append(words[i]+' '+words[i+1])
+    splitted_text.append(temp_str)
+    print(f"Splitted Text: {splitted_text}")
+    return splitted_text, start_times
 
 
 def app():
+    option = st.selectbox(
+        'Choose target language',
+        langs)
+    st.write('You selected:', option)
+
     # Upload video
     file = st.file_uploader("Upload video", type="mp4")
 
     if file:
+        os.makedirs(tmpDir, exist_ok=True)
+
         # Read video
         video_bytes = file.read()
 
@@ -54,46 +83,35 @@ def app():
 
         # Extract audio 
         temp_clip = mp.VideoFileClip(temp_input_video_path)
-        temp_clip.audio.write_audiofile("google_cloud.mp3")    
 
+        translate_language_code, voice_language_code = get_language_codes(option)
+        translated_audio = os.path.join("language_files", f"{translate_language_code}.wav")
+
+        temp_clip.audio.write_audiofile(temp_source_audio_path)
         # Extract text and timestamps
-        with open("google_cloud.mp3", 'rb') as fd:
+        with open(temp_source_audio_path, 'rb') as fd:
             enc = fd.read()
 
         text = speech_to_text(enc)
+        transcript = ""
+        for r in text.results:
+            transcript += r.alternatives[0].transcript
+        start_time = (text.results[0].alternatives[0].words[0].end_time).total_seconds() - 1
+        end_time = text.results[-1].result_end_time.total_seconds()
         
-        transcript = text.results[0].alternatives[0].transcript
-        splitted_texts = split_text(transcript)
-        splitted_texts = [translate_text("en", splitted_text) for splitted_text in splitted_texts]
-        eng_voice_paths = [text_to_speech(idx,splitted_text) for idx,splitted_text in enumerate(splitted_texts)]
-        new_audio = [process_audio(AudioFileClip(eng_voice_path),temp_clip.audio) for eng_voice_path in eng_voice_paths]
+        translated_text = translate_text(translate_language_code, transcript)
 
-        # new_audio = process_audio(AudioFileClip(eng_voice_paths[0]),temp_clip.audio)
-        # video_split_interval = temp_clip.duration // len(splitted_texts)
+        if os.path.isfile(translated_audio):
+            new_audio = AudioFileClip(translated_audio)
+        else:
+            eng_voice_path = text_to_speech(voice_language_code, tmpDir, 0, translated_text)
+            new_audio = process_audio(eng_voice_path,temp_clip.audio.subclip(start_time, end_time))
+            new_audio = resize_audio(new_audio, end_time-start_time)
 
-        # start_sec = 0
-        # end_sec = video_split_interval
-        
-        # new_audio = []
-        # while (start_sec < temp_clip.duration):
-        #     # Split and process audio
-        #     new_audio.append(process_audio(temp_clip.audio.subclip(start_sec, end_sec)))
+            start_silence = temp_clip.audio.subclip(0, start_time)
+            new_audio = mp.concatenate_audioclips([start_silence, new_audio])
 
-        #     start_sec += video_split_interval
-        #     end_sec += video_split_interval
-
-        #     if (end_sec > temp_clip.duration):
-        #         end_sec = temp_clip.duration
-
-        new_audio = mp.concatenate_audioclips(new_audio)
-
-
-        # for audio_chunk in temp_clip.audio.iter_chunks(chunk_duration = 1, nbytes=2,fps=44100):
-        #     print(audio_chunk.shape)
-        #     # new_chunk = AudioArrayClip(audio_chunk, fps=44100)
-        #     # new_chunk = process_audio(new_chunk)
-        #     # new_audio.append(new_chunk)
-        # new_audio = mp.concatenate_audioclips(new_audio)
+            new_audio.write_audiofile(translated_audio)
 
         # Overlap video with new audio
         final_clip = temp_clip.set_audio(new_audio)
@@ -107,6 +125,14 @@ def app():
                 st.header("Original Video")
                 st.video(video_bytes)
 
+                st.subheader('Original Text:')
+                st.write(transcript)
+
             with col2:
                 st.header("Processed Video")
                 st.video(temp_output_video_path)
+
+                st.subheader('Translated Text')
+                st.write(translated_text)
+        
+        shutil.rmtree(tmpDir)
--- pages/google_api_handler.py
+++ pages/google_api_handler.py
@@ -1,8 +1,10 @@
 import os
-from google.cloud import speech_v1, texttospeech 
-from googletrans import Translator
+import six
+import re
+from google.cloud import speech_v1, texttospeech, translate_v2
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="tartanhacks2022-340415-76005c636c36.json"
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=""
 
 
 def speech_to_text(audio):
@@ -14,10 +16,7 @@ def speech_to_text(audio):
     return response
 
 def translate_text(target, text):
-    import six
-    from google.cloud import translate_v2 as translate
-
-    translate_client = translate.Client()
+    translate_client = translate_v2.Client()
 
     if isinstance(text, six.binary_type):
         text = text.decode("utf-8")
@@ -26,35 +25,320 @@ def translate_text(target, text):
     # will return a sequence of results for each text.
     result = translate_client.translate(text, target_language=target)
 
+    translated_text = re.sub(r"&#39;", "'", result["translatedText"])    
+
     print(u"Text: {}".format(result["input"]))
-    print(u"Translation: {}".format(result["translatedText"]))
+    print(u"Translation: {}".format(translated_text))
     print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
-    
 
-    return result["translatedText"]
+    return translated_text
 
 
-# def translate_text(text):
-#     translator = Translator()
-#     result = translator.translate(text, dest="en")
-#     return result[0]
-
-def text_to_speech(idx, text):
+def text_to_speech(language_code, tmp_dir, idx, text):
     # Instantiates a client
     client = texttospeech.TextToSpeechClient()
     
     # Set the text input to be synthesized
     synthesis_input = texttospeech.SynthesisInput(text=text)
+    
     voice = texttospeech.VoiceSelectionParams(
-        language_code='en-US',
-        name='en-US-Wavenet-C',
-        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL)
-        # ssml_gender=texttospeech.SsmlVoiceGender.MALE)
+        language_code=language_code,
+        # name='en-US-Wavenet-D',
+        ssml_gender=texttospeech.SsmlVoiceGender.MALE
+    )
+        # ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL)
     audio_config = texttospeech.AudioConfig(
         audio_encoding=texttospeech.AudioEncoding.MP3)
     response = client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
-    with open(f'google_{idx}.wav', 'wb') as out:
-        out.write(response.audio_content)
-        print(f'Audio content written to file google_{idx}.mp3')
 
-    return f"google_{idx}.wav"
+    save_path = os.path.join(tmp_dir, f"google_{idx}.wav")
+    with open(save_path, 'wb') as out:
+        out.write(response.audio_content)
+        print(f'Audio content written to file {save_path}')
+
+    return save_path
+
+langs = ("Albanian",
+"Afrikaans",
+"Amharic",
+"Arabic",
+"Armenian",
+"Azerbaijani",
+"Basque",
+"Bengali",
+"Bosnian",
+"Bulgarian",
+"Catalan",
+"Chinese ",
+"Croatian",
+"Czech",
+"Danish",
+"Dutch",
+"English",
+"Estonian",
+"Finnish",
+"French",
+"Galician",
+"Georgian",
+"German",
+"Greek",
+"Gujarati",
+"Hebrew",
+"Hindi",
+"Hungarian",
+"Icelandic",
+"Indonesian",
+"Italian",
+"Japanese",
+"Javanese",
+"Kannada",
+"Kazakh",
+"Khmer",
+"Korean",
+"Lao",
+"Latvian",
+"Lithuanian",
+"Macedonian",
+"Malay",
+"Malayalam",
+"Marathi",
+"Mongolian",
+"Nepali",
+"Norwegian",
+"Persian",
+"Polish",
+"Portuguese ",
+"Punjabi",
+"Romanian",
+"Russian",
+"Serbian",
+"Sinhala",
+"Slovak",
+"Slovenian",
+"Spanish",
+"Sundanese",
+"Swahili",
+"Swedish",
+"Tamil",
+"Telugu",
+"Thai",
+"Turkish",
+"Ukrainian",
+"Urdu",
+"Uzbek",
+"Vietnamese",
+"Zulu")
+
+def get_language_codes(language):
+    if language == "Albanian":
+        translate_language_code = "sq"
+        voice_language_code = "sq-AL"
+    elif language == "Afrikaans":
+        translate_language_code = "af"
+        voice_language_code = "af-ZA"
+    elif language == "Amharic":
+        translate_language_code = "am"
+        voice_language_code = "am-ET"
+    elif language == "Arabic":
+        translate_language_code = "ar"
+        voice_language_code = "ar-EG"
+    elif language == "Armenian":
+        translate_language_code = "hy"
+        voice_language_code = "hy-AM"
+    elif language == "Azerbaijani":
+        translate_language_code = "az"
+        voice_language_code = "az-AZ"
+    elif language == "Basque":
+        translate_language_code = "eu"
+        voice_language_code = "eu-ES"
+    elif language == "Bengali":
+        translate_language_code = "bn"
+        voice_language_code = "bn-IN"
+    elif language == "Bosnian":
+        translate_language_code = "bs"
+        voice_language_code = "bs-BA"
+    elif language == "Bulgarian":
+        translate_language_code = "bg"
+        voice_language_code = "bg-BG"
+    elif language == "Catalan":
+        translate_language_code = "ca"
+        voice_language_code = "ca-ES"
+    elif language == "Chinese ":
+        translate_language_code = "zh"
+        voice_language_code = "zh"
+    elif language == "Croatian":
+        translate_language_code = "hr"
+        voice_language_code = "hr-HR"
+    elif language == "Czech":
+        translate_language_code = "cs"
+        voice_language_code = "cs-CZ"
+    elif language == "Danish":
+        translate_language_code = "da"
+        voice_language_code = "da-DK"
+    elif language == "Dutch":
+        translate_language_code = "nl"
+        voice_language_code = "nl-NL"
+    elif language == "English":
+        translate_language_code = "en"
+        voice_language_code = "en-US"
+    elif language == "Estonian":
+        translate_language_code = "et"
+        voice_language_code = "et-EE"
+    elif language == "Finnish":
+        translate_language_code = "fi"
+        voice_language_code = "fi-FI"
+    elif language == "French":
+        translate_language_code = "fr"
+        voice_language_code = "fr-FR"
+    elif language == "Galician":
+        translate_language_code = "gl"
+        voice_language_code = "gl-ES"
+    elif language == "Georgian":
+        translate_language_code = "ka"
+        voice_language_code = "ka-GE"
+    elif language == "German":
+        translate_language_code = "de"
+        voice_language_code = "de-DE"
+    elif language == "Greek":
+        translate_language_code = "el"
+        voice_language_code = "el-GR"
+    elif language == "Gujarati":
+        translate_language_code = "gu"
+        voice_language_code = "gu-IN"
+    elif language == "Hebrew":
+        translate_language_code = "he"
+        voice_language_code = "he-IL" 
+    elif language == "Hindi":
+        translate_language_code = "hi"
+        voice_language_code = "hi-IN"
+    elif language == "Hungarian":
+        translate_language_code = "hu"
+        voice_language_code = "hu-HU" 
+    elif language == "Icelandic":
+        translate_language_code = "ic"
+        voice_language_code = "ic-IS"
+    elif language == "Indonesian":
+        translate_language_code = "in"
+        voice_language_code = "in-ID" 
+    elif language == "Italian":
+        translate_language_code = "it"
+        voice_language_code = "it-KE" 
+    elif language == "Japanese":
+        translate_language_code = "ja"
+        voice_language_code = "ja-JP" 
+    elif language == "Javanese":
+        translate_language_code = "jv"
+        voice_language_code = "jv-ID" 
+    elif language == "Kannada":
+        translate_language_code = "kn"
+        voice_language_code = "kn-IN" 
+    elif language == "Kazakh":
+        translate_language_code = "kk"
+        voice_language_code = "kk-KZ" 
+    elif language == "Khmer":
+        translate_language_code = "km"
+        voice_language_code = "km-KH" 
+    elif language == "Korean":
+        translate_language_code = "ko"
+        voice_language_code = "ko-KR"
+    elif language == "Lao":
+        translate_language_code = "lo"
+        voice_language_code = "lo-LA"
+    elif language == "Latvian":
+        translate_language_code = "lv"
+        voice_language_code = "lv-LV"
+    elif language == "Lithuanian":
+        translate_language_code = "lt"
+        voice_language_code = "lt-LT"
+    elif language == "Macedonian":
+        translate_language_code = "mk"
+        voice_language_code = "mk-MK"
+    elif language == "Malay":
+        translate_language_code = "ms"
+        voice_language_code = "ms-MY"
+    elif language == "Malayalam":
+        translate_language_code = "ml"
+        voice_language_code = "ml-IN"
+    elif language == "Marathi":
+        translate_language_code = "mr"
+        voice_language_code = "mr-IN"
+    elif language == "Mongolian":
+        translate_language_code = "mn"
+        voice_language_code = "mn-MN"
+    elif language == "Nepali":
+        translate_language_code = "ne"
+        voice_language_code = "ne-NP"
+    elif language == "Norwegian":
+        translate_language_code = "no"
+        voice_language_code = "no-NO"
+    elif language == "Persian":
+        translate_language_code = "pe"
+        voice_language_code = "pe-IR"
+    elif language == "Polish":
+        translate_language_code = "pl"
+        voice_language_code = "pl-PL"
+    elif language == "Portuguese ":
+        translate_language_code = "pr"
+        voice_language_code = "pr-PT"
+    elif language == "Punjabi":
+        translate_language_code = "pa"
+        voice_language_code = "pa-Guru-IN"
+    elif language == "Romanian":
+        translate_language_code = "ro"
+        voice_language_code = "ro-RO"
+    elif language == "Russian":
+        translate_language_code = "ru"
+        voice_language_code = "ru-RU"
+    elif language == "Serbian":
+        translate_language_code = "sr"
+        voice_language_code = "sr-RS"
+    elif language == "Sinhala":
+        translate_language_code = "si"
+        voice_language_code = "si-LK"
+    elif language == "Slovak":
+        translate_language_code = "sk"
+        voice_language_code = "sk-SK"
+    elif language == "Slovenian":
+        translate_language_code = "sl"
+        voice_language_code = "sl-SI"
+    elif language == "Spanish":
+        translate_language_code = "es"
+        voice_language_code = "es-ES"
+    elif language == "Sundanese":
+        translate_language_code = "su"
+        voice_language_code = "su-ID"
+    elif language == "Swahili":
+        translate_language_code = "sw"
+        voice_language_code = "sw-KE"
+    elif language == "Swedish":
+        translate_language_code = "sv"
+        voice_language_code = "sv-SE"
+    elif language == "Tamil":
+        translate_language_code = "ta"
+        voice_language_code = "ta-IN"
+    elif language == "Telugu":
+        translate_language_code = "te"
+        voice_language_code = "te-IN"
+    elif language == "Thai":
+        translate_language_code = "th"
+        voice_language_code = "th-TH"
+    elif language == "Turkish":
+        translate_language_code = "tr"
+        voice_language_code = "tr-TR"
+    elif language == "Ukrainian":
+        translate_language_code = "uk"
+        voice_language_code = "uk-UA"
+    elif language == "Urdu":
+        translate_language_code = "ur"
+        voice_language_code = "ur-PK"
+    elif language == "Uzbek":
+        translate_language_code = "uz"
+        voice_language_code = "uz-UZ"
+    elif language == "Vietnamese":
+        translate_language_code = "vi"
+        voice_language_code = "vi-VN"
+    elif language == "Zulu":
+        translate_language_code = "zu"
+        voice_language_code = "zu-ZA"
+
+    return translate_language_code, voice_language_code
